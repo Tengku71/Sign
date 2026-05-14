e71d05b83ba0ffce63d670268a5ec4746b84d3c7
Tengku Dimas <tengkudimasadt@gmail.com>
2026-05-14 12:59:01 +0700

    update 1.1 svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from "@sveltejs/adapter-node";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
     runes: ({ filename }) => (filename.split(/[/\\]/).includes("node_modules") ? undefined : true),
   },
   kit: {
     adapter: adapter(),
-    paths: {
-      base: "",
-      relative: false,
-    },
   },
 };
 
