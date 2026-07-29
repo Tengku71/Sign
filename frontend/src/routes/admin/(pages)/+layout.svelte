a528c979024327e94e5137d3e4f086cb56f50865
Tengku Dimas <tengkudimasadt@gmail.com>
2026-07-29 10:32:50 +0700

    update

--- frontend/src/routes/admin/(pages)/+layout.svelte
+++ frontend/src/routes/admin/(pages)/+layout.svelte
@@ -5,7 +5,7 @@
   import { getAdminProfile, getAllUsers } from "$lib/admin/getAdmin";
   import { adminStore, usersStore } from "$lib/stores";
   import { goto } from "$app/navigation";
-  import Skeleton from "$lib/components/Skeleton.svelte";
+  // import Skeleton from "$lib/components/Skeleton.svelte";
   let { children } = $props();
 
   let isLoggedIn = $state(false);
@@ -33,14 +33,11 @@
   });
 </script>
 
-{#if loading}
-  <!-- <Skeleton /> -->
-{:else if isLoggedIn}
-  <div class="h-screen flex overflow-hidden bg-linear-to-br from-slate-900 via-slate-800 to-indigo-950">
-    <SideNav {data} />
+<!-- <Skeleton /> -->
+<div class="h-screen flex overflow-hidden bg-linear-to-br from-slate-900 via-slate-800 to-indigo-950">
+  <SideNav {data} />
 
-    <main class="flex-1 overflow-y-auto">
-      {@render children()}
-    </main>
-  </div>
-{/if}
+  <main class="flex-1 overflow-y-auto">
+    {@render children()}
+  </main>
+</div>
